743e662860720826171b79095975f561206f2a9f
Athena-Jade <athenajade72@gmal.com>
2024-01-04 10:15:56 +0100

    install reactjs and tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>App
+       <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
+     
+       <div className='text-red-500'>ok</div>
+    </div>
+  )
+}
+
